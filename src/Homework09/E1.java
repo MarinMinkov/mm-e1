864ee5ca70829fe10c7b67251477c54a93882ef7
marinminkov <marin.minkov@mentormate.com>
2022-05-10 08:57:33 +0300

    Added more methods to the person class.

--- src/Homework09/E1.java
+++ src/Homework09/E1.java
@@ -1,5 +1,6 @@
 package Homework09;
 
+import javax.sound.midi.Soundbank;
 import java.util.InputMismatchException;
 
 public class E1 {
@@ -36,8 +37,8 @@ public class E1 {
             try
             {
 
-                sex = sex.toLowerCase();
-                if (sex == "male" || sex == "female") {
+                String tempSex = sex.toLowerCase();
+                if (tempSex.equals("male") || tempSex.equals("female") ) {
                     return sex;
                 } else {
                     throw new InputMismatchException("Sex must be Male or Female");
@@ -63,12 +64,113 @@ public class E1 {
             this.job = job;
             this.country = country;
         }
+
+        public void sayHello()
+        {
+            String tempLang = language.toLowerCase();
+            switch (tempLang)
+            {
+                case "bulgarian":
+                    System.out.println("Здравей");
+                    break;
+                case "italian":
+                    System.out.println("Ciao");
+                    break;
+                default:
+                    System.out.println("Hello");
+                    break;
+
+            }
+        }
+
+        public void celebrateEaster() // could be shorter if we check for Islam and everyone "else" celebrates.
+        {
+            String tempReligion = religion.toLowerCase();
+            if (tempReligion.equals("christian"))
+            {
+                System.out.println("This person celebrates Easter");
+            }
+            else if (tempReligion.equals("orthodox"))
+            {
+                System.out.println("This person celebrates Easter");
+            }
+            else if (tempReligion.equals("catholic"))
+            {
+                System.out.println("This person celebrates Easter");
+            }
+            else
+            {
+                System.out.println("This person doesn't celebrates Easter");
+            }
+        }
+
+        public void isAdult()
+        {
+            String tempCountry = country.toLowerCase();
+            switch (tempCountry)
+            {
+                case "usa":
+                {
+                    if (age>21)
+                    {
+                        System.out.println("Adult");
+                    }
+                    else
+                    {
+                        System.out.println("Not Adult");
+                    }
+                }
+                break;
+                default:
+                {
+                    if (age>18)
+                    {
+                        System.out.println("Adult");
+                    }
+                    else
+                    {
+                        System.out.println("Not Adult");
+                    }
+                }
+                break;
+            }
+        }
+
+        public void canTakeLoan()
+        {
+            String tempJob = job.toLowerCase();
+            if (tempJob.equals("unemployed"))
+            {
+                System.out.println("Can't take loan");
+            }
+            else
+            {
+                System.out.println("Can take loan");
+            }
+        }
     }
+
+
 
     public static void main(String[] args) {
 
-        Person obj = new Person("Marin", "Male", "Christian", "Bulgarian", "Bulgarian", "9308158526", "QA", "Bulgaria");
+        Person obj1 = new Person("Marin", "Male", "Christian", "Bulgarian", "Bulgarian", "9308158526", "Unemployed", "Bulgaria");
+        Person obj2 = new Person("Marin", "Male", "Orthodox", "Bulgarian", "Bulgarian", "9308158526", "QA", "Bulgaria");
+        Person obj3 = new Person("Marin", "Male", "Islam", "English", "USA", "9308158526", "QA", "USA");
 
+        obj1.sayHello();
+        obj1.celebrateEaster();
+        obj1.isAdult();
+        obj1.canTakeLoan();
 
+        obj2.sayHello();
+        obj2.celebrateEaster();
+        obj2.isAdult();
+        obj2.canTakeLoan();
+
+        obj3.sayHello();
+        obj3.celebrateEaster();
+        obj3.isAdult();
+        obj3.canTakeLoan();
     }
 }
